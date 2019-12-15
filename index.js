288e96d1131999ecc7b6ed1f1d51db7ea816ffd9
ilyazkm <ilyaz.k.m@gmail.com>
2019-12-15 00:58:35 -0800

    Substraction function added to new branch

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
-function add(a,b){
-    return a+b;
+function sub(a,b){
+    return a-b;
 }
-let sum=add(2,3);
-console.log("The Sum is",sum);
+let result=sub(2,3);
+console.log("The Result is",result);
