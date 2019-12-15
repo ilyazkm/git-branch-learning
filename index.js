054edbcf1bde4e2953ed47795ce081c5a26ce117
ilyazkm <ilyaz.k.m@gmail.com>
2019-12-15 02:40:12 -0800

    Higher-Order-Function

--- index.js
+++ index.js
@@ -1,5 +1,25 @@
-function add(a,b){
-    return a+b;
+// function add(a,b){
+//     return a+b;
+// }
+// let sum=add(2,3);
+// console.log("The Sum is",sum);
+// function say(){
+//     console.log("Hello");
+// }
+
+// function call(){
+//     say();
+//     console.log("Hi");
+// }
+// call();
+function add(x,y) {
+    return x+y;
 }
-let sum=add(2,3);
-console.log("The Sum is",sum);
+var sum=add(3,6);
+console.log(sum);
+// var a=5;
+// function b(){ 
+//     let c=4;
+//     console.log(a);
+// }
+// b();
